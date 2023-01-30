d94693c912d037950586afaf97a2056826c2c2f0
EmberTSeal <trishaseal49@gmail.com>
2023-01-30 22:12:28 +0530

    Desktop Design
    adjust margins and paddings
    make image change based on screen change using html

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-$(window).resize(function(e){
-    if($(window).width() < 768) {
-    console.log($(window).width());
-       $("#mainArticleImg").each(function() {
-       $(this).attr("src", "assets/images/image-web-3-mobile.jpg");
-                 });
-             } else if ($(window).width() >= 768) {
-                 $("#mainArticleImg").each(function() {
-                 $(this).attr("src","assets/images/image-web-3-desktop.jpg");
-                 });                        
-     }         
- });
-
 $(document).ready(function () {
     $('.navbar-toggler-icon').click(function () {
         $('.navbar-toggler-icon').toggleClass('navbar-toggled-on');
-    })    
+    })
 });
